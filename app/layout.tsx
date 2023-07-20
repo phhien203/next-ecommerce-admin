aeeea09fb60f10f62a7af8b6368de36f0018809c
Pham Huu Hien <phhien203@gmail.com>
2023-07-20 22:22:10 +0700

    add Clerk authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'E-commerce Admin Dashboard',
+  title: 'ShopNow Admin Dashboard',
   description: 'An e-commerce admin dashboard built in NextJS',
 }
 
@@ -15,8 +16,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   )
 }
