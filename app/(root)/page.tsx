d6e1ec81edfad94fc20dee116069204cf5934d1f
Pham Huu Hien <phhien203@gmail.com>
2023-07-20 22:00:31 +0700

    add button component

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  return (
+    <div className="p-4">
+      <Button variant="ghost">Click me</Button>
+    </div>
+  )
+}
