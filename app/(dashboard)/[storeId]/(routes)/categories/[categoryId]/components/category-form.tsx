d3850485e1757afdfe215f10cb15a9e95c677754
Pham Huu Hien <phhien203@gmail.com>
2023-07-22 09:02:45 +0700

    feat: build UI for category

---
+++ app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/components/category-form.tsx
@@ -0,0 +1,211 @@
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Billboard, Category } from '@prisma/client'
+import axios from 'axios'
+import { TrashIcon } from 'lucide-react'
+import { useParams, useRouter } from 'next/navigation'
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+
+import AlertModal from '@/components/modals/alert-modal'
+import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import Heading from '@/components/ui/heading'
+import { Input } from '@/components/ui/input'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import { Separator } from '@/components/ui/separator'
+import { useToast } from '@/components/ui/use-toast'
+
+const formSchema = z.object({
+  name: z.string().min(1),
+  billboardId: z.string().min(1),
+})
+
+type CategoryFormValues = z.infer<typeof formSchema>
+
+interface CategoryFormProps {
+  initialData: Category | null
+  billboards: Billboard[]
+}
+
+export default function CategoryForm({
+  initialData,
+  billboards,
+}: CategoryFormProps) {
+  const params = useParams()
+  const router = useRouter()
+
+  const title = initialData ? 'Edit category' : 'Create category'
+  const description = initialData ? 'Edit a category' : 'Create a new category'
+  const toastMessage = initialData ? 'Category updated.' : 'Category created.'
+  const action = initialData ? 'Save changes' : 'Create'
+
+  const { toast } = useToast()
+  const [open, setOpen] = React.useState(false)
+  const [loading, setLoading] = React.useState(false)
+
+  const form = useForm<CategoryFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData || {
+      name: '',
+      billboardId: '',
+    },
+  })
+
+  const onSubmit = async (data: CategoryFormValues) => {
+    try {
+      setLoading(true)
+
+      if (initialData) {
+        await axios.patch(
+          `/api/${params.storeId}/categories/${params.categoryId}`,
+          data,
+        )
+      } else {
+        await axios.post(`/api/${params.storeId}/categories`, data)
+      }
+      router.refresh()
+      router.push(`/${params.storeId}/categories`)
+      toast({
+        description: toastMessage,
+      })
+    } catch (error) {
+      toast({
+        variant: 'destructive',
+        description: 'Something went wrong',
+      })
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const onDelete = async () => {
+    try {
+      setLoading(true)
+      await axios.delete(
+        `/api/${params.storeId}/categories/${params.categoryId}`,
+      )
+      router.refresh()
+      router.push(`/${params.storeId}/categories`)
+    } catch (error) {
+      toast({
+        variant: 'destructive',
+        title: 'Cannot remove category',
+        description:
+          'Make sure you removed all products using this category first.',
+      })
+    } finally {
+      setOpen(false)
+      setLoading(false)
+    }
+  }
+
+  return (
+    <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
+      <div className="flex items-center justify-between">
+        <Heading title={title} description={description} />
+
+        {initialData && (
+          <Button
+            disabled={loading}
+            variant="destructive"
+            size="icon"
+            onClick={() => {
+              setOpen(true)
+            }}
+          >
+            <TrashIcon className="h-4 w-4" />
+          </Button>
+        )}
+      </div>
+
+      <Separator />
+
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="Category name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="billboardId"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Billboard</FormLabel>
+                  <Select
+                    disabled={loading}
+                    value={field.value}
+                    defaultValue={field.value}
+                    onValueChange={field.onChange}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue
+                          defaultValue={field.value}
+                          placeholder="Select a billboard"
+                        ></SelectValue>
+                      </SelectTrigger>
+                    </FormControl>
+
+                    <SelectContent>
+                      {billboards.map((billboard) => (
+                        <SelectItem key={billboard.id} value={billboard.id}>
+                          {billboard.label}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <Button type="submit" disabled={loading}>
+            {action}
+          </Button>
+        </form>
+      </Form>
+    </>
+  )
+}
